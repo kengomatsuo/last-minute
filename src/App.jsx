1c34cb20a856ba6c7bc53070213c684ba514b638
kengomatsuo <kennethfang1000@gmail.com>
2025-02-19 17:47:19 +0700

    refactor: update App component layout and styling, replace logo with image

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import './App.css'
+import ket from "./assets/ket.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
+      <div className="header">
+        {/* Import image of ket.png from assets */}
+        <img src={ket} width={100} alt="Ket" style={{ filter: "invert(1)" }} />
+        <h1 style={{marginTop: -9}}>Last Minute</h1>
       </div>
-      <h1>Last Minute</h1>
       <div className="card">
-        <p>
-          Initialized main page for the Last Minute project.
-        </p>
+        <p>Initialized main page for the Last Minute project.</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
