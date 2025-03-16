82529eacdf7aa97dc599cc67ce134135c4d23aab
kengomatsuo <kennethfang1000@gmail.com>
2025-03-16 20:10:08 +0700

    feat: integrate framer-motion for animated menu transitions in CustomNavBar and enhance responsiveness

--- src/components/CustomNavBar.jsx
+++ src/components/CustomNavBar.jsx
@@ -1,4 +1,5 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import CustomButton from "./CustomButton";
 import { NavLink } from "react-router-dom";
 import { ScreenContext } from "../contexts/ScreenContext";
@@ -8,6 +9,12 @@ import MenuBurger from "../assets/icons/menu-burger.svg?react";
 const CustomNavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { isSmallScreen } = useContext(ScreenContext);
+
+  useEffect(() => {
+    if (isSmallScreen) {
+      setIsMenuOpen(false);
+    }
+  }, [isSmallScreen]);
 
   const navigationPaths = [
     { name: "Link", path: "/1" },
@@ -22,6 +29,7 @@ const CustomNavBar = () => {
           Last Minute
         </NavLink>
       </div>
+
       {isSmallScreen ? (
         <>
           <CustomHyperlink
@@ -30,42 +38,78 @@ const CustomNavBar = () => {
           >
             <MenuBurger width={24} height={24} />
           </CustomHyperlink>
-          {isMenuOpen ? (
-            <>
-              <div className="absolute top-0 left-0 w-screen h-screen bg-background/75" />
-              <div className="w-72 max-w-4/5 fixed top-0 text-end gap-2 px-6 py-4 right-0 h-screen bg-background shadow-[-2px_0px_6px_0px_rgba(0,_0,_0,_0.1)]">
-                <CustomHyperlink
-                  className="w-min aspect-square flex items-center justify-center ml-auto"
-                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+
+          {/* Animate Presence for smooth entry and exit */}
+          <AnimatePresence>
+            {isMenuOpen && (
+              <>
+                <motion.div
+                  className="absolute top-0 left-0 w-screen h-screen bg-background-secondary/25"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  onClick={() => setIsMenuOpen(false)}
+                />
+
+                <motion.div
+                  className="w-72 max-w-4/5 fixed top-0 text-end flex flex-col gap-1 px-6 py-4 right-0 h-screen bg-background"
+                  initial={{
+                    x: "100%",
+                    boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)",
+                  }}
+                  animate={{
+                    x: 0,
+                    boxShadow: "3px 0px 10px 2px rgba(0, 0, 0, 0.1)",
+                  }}
+                  exit={{
+                    x: "100%",
+                    boxShadow: "0px 0px 0px 0px rgba(0, 0, 0, 0)",
+                  }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 200,
+                    damping: 20,
+                    mass: 0.5,
+                  }}
                 >
-                  <MenuBurger width={24} height={24} />
-                </CustomHyperlink>
-                Navigation
-                {navigationPaths.map((navPath) => (
-                  <NavLink
-                    key={navPath.path}
-                    to={navPath.path}
-                    className={({ isActive }) =>
-                      isActive
-                        ? "underline underline-offset-4 underline-primary"
-                        : undefined
-                    }
+                  <CustomHyperlink
+                    className="w-min aspect-square flex items-center justify-center ml-auto"
+                    onClick={() => setIsMenuOpen(false)}
                   >
-                    <CustomHyperlink className="text-right">
-                      {navPath.name}
-                    </CustomHyperlink>
-                  </NavLink>
-                ))}
-              </div>
-            </>
-          ) : null}
+                    <MenuBurger width={24} height={24} />
+                  </CustomHyperlink>
+
+                    <NavLink to={"/"} className="flex">
+                      <CustomButton>Register</CustomButton>
+                    </NavLink>
+                    <NavLink to={"/login"} className="flex">
+                      <CustomButton filled>Login</CustomButton>
+                    </NavLink>
+
+                  <p className="text-lg font-semibold mt-2 mr-3">Navigation</p>
+
+                  {navigationPaths.map((navPath) => (
+                    <NavLink
+                      key={navPath.path}
+                      to={navPath.path}
+                      className={({ isActive }) =>
+                        isActive
+                          ? "underline underline-offset-4 underline-primary"
+                          : undefined
+                      }
+                    >
+                      <CustomHyperlink className="text-right block py-2">
+                        {navPath.name}
+                      </CustomHyperlink>
+                    </NavLink>
+                  ))}
+                </motion.div>
+              </>
+            )}
+          </AnimatePresence>
         </>
       ) : (
-        <div
-          className={
-            "text-lg font-medium items-center inline-flex top-0 py-0 gap-4"
-          }
-        >
+        <div className="text-lg font-medium items-center inline-flex top-0 py-0 gap-4">
           <div className="inline-flex gap-2">
             {navigationPaths.map((navPath) => (
               <NavLink
@@ -82,7 +126,7 @@ const CustomNavBar = () => {
             ))}
           </div>
 
-          <div className="flex gap-2 max-md:flex-col flex-row min-w-50">
+          <div className="flex gap-2 flex-row min-w-50">
             <NavLink to={"/"} className="flex-1 flex">
               <CustomButton>Register</CustomButton>
             </NavLink>
