0b941d61b81b5302ed966baf1a3b7ecb7c312274
kengomatsuo <kennethfang1000@gmail.com>
2025-03-17 18:49:09 +0700

    feat: add PWA support with vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  name: "Last Minute",
+  short_name: "Last Minute",
+  description: "A versatile app for impromptu studies",
+  display: "standalone",
+  start_url: "/last-minute/",
+  // scope: "/last-minute/",
+  // icons: [
+  //   {
+  //     src: "/pwa-192x192.png",
+  //     sizes: "192x192",
+  //     type: "image/png",
+  //   },
+  //   {
+  //     src: "/pwa-512x512.png",
+  //     sizes: "512x512",
+  //     type: "image/png",
+  //   },
+  // ],
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/last-minute/",
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), VitePWA(manifestForPlugin)],
   build: {
     rollupOptions: {
       output: {
