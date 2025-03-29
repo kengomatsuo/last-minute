b2a5f1f02e54815de3fd702b2f0575a9ade40560
kengomatsuo <kennethfang1000@gmail.com>
2025-03-29 10:52:35 +0700

    ✨ Enhance VideoCall component with audio and video controls
    - Added icons for video and audio streaming toggle
    - Refactored state management for video and audio streaming
    - Updated stream handling to support toggling of audio and video
    - Improved cleanup process for media streams
    
    This update allows users to easily control their audio and video
    streams during a call, enhancing the overall user experience.

--- src/components/VideoCall.jsx
+++ src/components/VideoCall.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState, useRef, useCallback } from 'react'
 import { useConsoleLog } from '../hooks'
 import CustomButton from './CustomButton'
+import VideoIcon from '../assets/icons/video-camera-alt.svg?react'
+import VideoSlashIcon from '../assets/icons/video-slash.svg?react'
+import AudioIcon from '../assets/icons/microphone.svg?react'
+import AudioSlashIcon from '../assets/icons/microphone-slash.svg?react'
 
 /**
  * VideoCall component for handling video and audio streaming
@@ -14,7 +18,8 @@ const VideoCall = ({ courseId }) => {
   const [audioInputs, setAudioInputs] = useState([])
   const [audioOutputs, setAudioOutputs] = useState([])
   const [stream, setStream] = useState(null)
-  const [isStreaming, setIsStreaming] = useState(false)
+  const [isVideoStreaming, setIsVideoStreaming] = useState(false)
+  const [isAudioStreaming, setIsAudioStreaming] = useState(false)
   const videoRef = useRef(null)
 
   useConsoleLog('videoInputs', videoInputs)
@@ -56,14 +61,13 @@ const VideoCall = ({ courseId }) => {
   /**
    * Stops all tracks in the current stream and cleans up
    */
-  const stopVideoStream = useCallback(() => {
+  const stopStream = useCallback(() => {
     if (stream) {
       stream.getTracks().forEach(track => track.stop())
       if (videoRef.current) {
         videoRef.current.srcObject = null
       }
       setStream(null)
-      setIsStreaming(false)
     }
   }, [stream])
 
@@ -72,52 +76,55 @@ const VideoCall = ({ courseId }) => {
    */
   useEffect(() => {
     return () => {
-      stopVideoStream()
+      stopStream()
     }
-  }, [stopVideoStream])
+  }, [stopStream])
 
   /**
    * Opens camera with specified settings
    *
    * @param {string} cameraId - The ID of the camera to use
-   * @param {number} minWidth - Minimum width for the video
-   * @param {number} minHeight - Minimum height for the video
+   * @param {number} width - Width for the video
+   * @param {number} height - Height for the video
    * @returns {Promise<MediaStream>} The media stream
    */
-  const openCamera = async (cameraId, minWidth, minHeight) => {
-    const constraints = {
-      audio: {
-        deviceId: activeMicrophoneId,
-        echoCancellation: true,
-      },
-      video: {
-        deviceId: cameraId,
-        width: { min: minWidth },
-        height: { min: minHeight },
-      },
-    }
-    return await navigator.mediaDevices.getUserMedia(constraints)
-  }
-
-  /**
-   * Toggles the video stream on/off
-   */
-  const toggleVideoStream = async () => {
-    try {
-      if (isStreaming) {
-        stopVideoStream()
-      } else {
-        const newStream = await openCamera(activeCameraId, 1280, 720)
-        if (videoRef.current) {
-          videoRef.current.srcObject = newStream
+  useEffect(() => {
+    const updateStream = async () => {
+      try {
+        if (stream) {
+          stopStream()
         }
-        setStream(newStream)
-        setIsStreaming(true)
+        if (isVideoStreaming || isAudioStreaming) {
+          const constraints = {
+            audio: isAudioStreaming
+              ? {
+                  deviceId: activeMicrophoneId,
+                  echoCancellation: true,
+                }
+              : false,
+            video: isVideoStreaming
+              ? {
+                  deviceId: activeCameraId,
+                  width: 1920,
+                  height: 1080,
+                }
+              : false,
+          }
+          console.log(constraints)
+          const newStream = await navigator.mediaDevices.getUserMedia(
+            constraints
+          )
+          if (videoRef.current) {
+            videoRef.current.srcObject = newStream
+          }
+          setStream(newStream)
+        }
+      } catch (error) {
+        console.error('Error opening camera:', error)
       }
-    } catch (error) {
-      console.error('Error handling video stream:', error)
     }
-  }
+    updateStream()
+  }, [isAudioStreaming, isVideoStreaming, activeCameraId, activeMicrophoneId])
 
   /**
    * Handles camera selection change
@@ -127,47 +134,46 @@ const VideoCall = ({ courseId }) => {
   const handleCameraChange = async e => {
     const newCameraId = e.target.value
     setActiveCameraId(newCameraId)
-
-    if (isStreaming) {
-      stopVideoStream()
-      const newStream = await openCamera(newCameraId, 1280, 720)
-      if (videoRef.current) {
-        videoRef.current.srcObject = newStream
-      }
-      setStream(newStream)
-      setIsStreaming(true)
-    }
   }
 
   /**
-    * Handles microphone selection change
-    *
-    * @param {Event} e - The change event
-    */
-    const handleMicrophoneChange = async e => {
-     const newMicrophoneId = e.target.value
-     setActiveMicrophoneId(newMicrophoneId)
-
-     if (isStreaming) {
-      stopVideoStream()
-      const newStream = await openCamera(activeCameraId, 1280, 720)
-      if (videoRef.current) {
-        videoRef.current.srcObject = newStream
-      }
-      setStream(newStream)
-      setIsStreaming(true)
-     }
-    }
+   * Handles microphone selection change
+   *
+   * @param {Event} e - The change event
+   */
+  const handleMicrophoneChange = async e => {
+    const newMicrophoneId = e.target.value
+    setActiveMicrophoneId(newMicrophoneId)
+  }
 
   return (
     <div className='bg-black flex-1 h-full relative flex justify-center items-center'>
       <video ref={videoRef} id='localVideo' autoPlay playsInline muted />
 
-      <div className='absolute bottom-4 flex gap-4 bg-background p-2 rounded-lg max-w-11/12 overflow-auto'>
-        <CustomButton onClick={toggleVideoStream} filled>
-          {isStreaming ? 'Close Camera' : 'Open Camera'}
+      <div className='absolute bottom-4 flex gap-4 bg-primary p-1 rounded-lg max-w-11/12 overflow-auto'>
+        <CustomButton
+          onClick={() => setIsVideoStreaming(!isVideoStreaming)}
+          filled
+          className='!p-2 flex aspect-square'
+        >
+          {isVideoStreaming ? (
+            <VideoIcon className=' w-6 h-6 fill-background' />
+          ) : (
+            <VideoSlashIcon className='w-6 h-6 fill-background' />
+          )}
         </CustomButton>
-        {videoInputs.length > 0 && (
+        <CustomButton
+          onClick={() => setIsAudioStreaming(!isAudioStreaming)}
+          filled
+          className='!p-2 flex aspect-square'
+        >
+          {isAudioStreaming ? (
+            <AudioIcon className=' w-6 h-6 fill-background' />
+          ) : (
+            <AudioSlashIcon className='w-6 h-6 fill-background' />
+          )}
+        </CustomButton>
+        {/* {videoInputs.length > 0 && (
           <div>
             <label htmlFor='cameraSelect'>Camera: </label>
             <select
@@ -182,8 +188,8 @@ const VideoCall = ({ courseId }) => {
               ))}
             </select>
           </div>
-        )}
-        {audioInputs.length > 0 && (
+        )} */}
+        {/* {audioInputs.length > 0 && (
           <div>
             <label htmlFor='microphoneSelect'>Microphone: </label>
             <select
@@ -199,7 +205,7 @@ const VideoCall = ({ courseId }) => {
               ))}
             </select>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   )
